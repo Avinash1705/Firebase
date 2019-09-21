584078a9f2be73647590173a9513e978851927ae
Avinash1705 <avinashrawat1705@gmail.com>
2019-09-21 23:57:37 +0530

    Admin power pass to all activity

---
+++ app/src/main/java/com/example/searchkaro/AdminPower/ChoosePageToEdit.java
@@ -0,0 +1,7 @@
+package com.example.searchkaro.AdminPower;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Admin_otp extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/example/searchkaro/SearchKaro.java
@@ -0,0 +1,14 @@
+package com.example.searchkaro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SearchKaro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_karo);
+    }
+}
