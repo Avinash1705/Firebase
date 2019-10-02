c39d1bc506bd6f0096763c9d1b81d32884a245cf
Avinash1705 <avinashrawat1705@gmail.com>
2019-10-02 22:45:03 +0530

    api added and upload error

---
+++ app/src/main/java/com/example/searchkaro/Bussiness_Registration/Bussiness_type_registration.java
@@ -0,0 +1,13 @@
+package com.example.searchkaro.Bussiness_Registration;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Bussiness_type_registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bussiness_type_registration);
+    }
+}
